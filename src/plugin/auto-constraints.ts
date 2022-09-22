0e84ca142f66b4d94f419dcd40b21e25268023db
James Bradford <j.bradford@pnnl.gov>
2022-09-21 17:27:34 -0700

    adding "don't allow Stretch" conditions

--- src/plugin/auto-constraints.ts
+++ src/plugin/auto-constraints.ts
@@ -16,7 +16,7 @@ export function frameAndAutoConstrainSelectionChildren() {
 	// new frame insertion point should be at the first selected node
 	const { selection } = figma.currentPage;
 	const insertionNode = selection[0].parent;
-	
+
 	// TODO: get correct insertionIndex
 	const insertionIndex = 0; // getIndexInParent(selection[0])
 	const groupNode = figma.group(selection, insertionNode, insertionIndex);
@@ -28,9 +28,11 @@ export function frameAndAutoConstrainSelectionChildren() {
 	frameNode.x = groupNode.x;
 	frameNode.y = groupNode.y;
 	frameNode.resize(groupNode.width, groupNode.height);
+	frameNode.fills = [];
+	frameNode.clipsContent = false;
 
 	// offset each child by the groupNode x y, or it gets double offset
-	// TODO: consider rotation and transforms
+	// TODO: consider rotation and transforms // maybe don't need to?
 	const offsetX = groupNode.x;
 	const offsetY = groupNode.y;
 
@@ -41,6 +43,7 @@ export function frameAndAutoConstrainSelectionChildren() {
 	});
 
 	// groupNode removes itself when it has no children
+	// groupNode.remove()
 
 	frameNode.children.forEach((childNode) => autoConstraints(childNode));
 }
@@ -49,6 +52,7 @@ const autoConstraints = (node: SceneNode) => {
 	const child = node;
 	const parent = child.parent;
 
+	// TODO:
 	// if child.type === 'GROUP', get children recursively - ignore for now
 	// if parent.type === 'GROUP', get parent recursively - ignore for now
 
@@ -79,20 +83,124 @@ const autoConstraints = (node: SceneNode) => {
 		end: parentSides.right,
 	};
 
-	const horizontal = autoConstraint(childHorizontal, parentHorizontal);
-	const vertical = autoConstraint(childVertical, parentVertical);
+	// 'STRETCH' seems pretty dangerous actually...
+
+	// don't apply 'STRETCH' to any object with constrained proportions
+	const noStretchAny = child.constrainProportions;
+
+	// don't apply 'STRETCH' to auto layouts that hug, or have no children that fill
+	const noStretchAutoLayoutHorizontal = noStretchAutoLayout(node, 'HORIZONTAL');
+	const noStretchAutoLayoutVertical = noStretchAutoLayout(node, 'VERTICAL');
+
+	// don't apply 'STRETCH' to text dynamic text sizing settings
+	const noStretchTextHorizontal = 'textAutoResize' in child && child.textAutoResize === 'WIDTH_AND_HEIGHT';
+	const noStretchTextVertical =
+		'textAutoResize' in child && (child.textAutoResize === 'WIDTH_AND_HEIGHT' || child.textAutoResize === 'HEIGHT');
+
+	const horizontal = autoConstraint(
+		childHorizontal,
+		parentHorizontal,
+		noStretchAny || noStretchTextHorizontal || noStretchAutoLayoutHorizontal
+	);
+	const vertical = autoConstraint(
+		childVertical,
+		parentVertical,
+		noStretchAny || noStretchTextVertical || noStretchAutoLayoutVertical
+	);
+
 	child.constraints = {
 		horizontal,
 		vertical,
 	};
 };
 
-function autoConstraint(childLine: Line, parentLine: Line): ConstraintType {
+function noStretchAutoLayout(node: SceneNode, direction: BaseFrameMixin['layoutMode']): boolean {
+	if (!('layoutMode' in node)) return false;
+	const {
+		layoutMode,
+		counterAxisSizingMode,
+		primaryAxisSizingMode,
+		primaryAxisAlignItems,
+		counterAxisAlignItems,
+		children,
+	} = node;
+	if (layoutMode == 'NONE') return false;
+	const isPrimaryAxis = direction === layoutMode;
+	// console.log({ node });
+	// console.log({
+	// 	direction,
+	// 	layoutMode,
+	// 	counterAxisSizingMode,
+	// 	primaryAxisSizingMode,
+	// 	primaryAxisAlignItems,
+	// 	isPrimaryAxis,
+	// 	children,
+	// });
+	if (isPrimaryAxis) {
+		// if spacing mode = space between, do stretch
+		if (primaryAxisAlignItems === 'SPACE_BETWEEN') return false;
+
+		// if resizing = hug, don't stretch
+		if (primaryAxisSizingMode === 'AUTO') return true;
+
+		// if any children resizing = fill, do stretch, else don't
+		return !allowStretchAutoLayoutChildren(children, isPrimaryAxis);
+	} else {
+		// if text baseline alignment is on, don't stretch
+		if (counterAxisAlignItems === 'BASELINE') return true;
+
+		// if resizing = hug, don't stretch
+		if (counterAxisSizingMode === 'AUTO') return true;
+
+		// if any children resizing = fill, do stretch, else don't
+		return !allowStretchAutoLayoutChildren(children, isPrimaryAxis);
+	}
+}
+function allowStretchAutoLayoutChildren(children: readonly SceneNode[], isPrimaryAxis:boolean): boolean {
+	console.log({ children });
+	return (
+		undefined !==
+		children.find((node) => {
+			if (!('layoutPositioning' in node)) return false;
+
+			const {
+				visible,
+				name,
+				layoutPositioning,
+				layoutAlign,
+				layoutGrow,
+			} = node;
+			console.log({
+				node,
+				isPrimaryAxis,
+				visible,
+				name,
+				layoutPositioning,
+				layoutAlign,
+				layoutGrow,
+			});
+
+			// if node is absolutely positioned, ignore it
+			if (node.layoutPositioning === 'ABSOLUTE') return false
+
+			// if node is hidden, ignore it
+			if (!visible) return false;
+
+			if (isPrimaryAxis) {
+				return layoutGrow > 0
+			} else {
+				return layoutAlign === 'STRETCH'
+			}
+		})
+	);
+}
+
+function autoConstraint(childLine: Line, parentLine: Line, noStretch: boolean = false): ConstraintType {
 	const parentLength = parentLine.end - parentLine.start;
 	const childLength = childLine.end - childLine.start;
 
 	if (isCentered(childLine, parentLine)) {
-		if (childLength < parentLength * 0.5) {
+		if (noStretch || childLength < parentLength * 0.5) {
 			return 'CENTER';
 		} else {
 			return 'STRETCH';
@@ -103,7 +211,7 @@ function autoConstraint(childLine: Line, parentLine: Line): ConstraintType {
 		const pinRight = childLine.end > parentLength * (1 - edgeTolerance);
 
 		if (pinLeft && pinRight) {
-			return 'STRETCH';
+			return noStretch ? 'CENTER' : 'STRETCH';
 		} else if (pinLeft) {
 			return 'MIN';
 		} else if (pinRight) {
@@ -148,6 +256,6 @@ function getSides(rect: Rect, parentRect: Rect = defaultRect): Sides {
 
 const defaultRect: Rect = { x: 0, y: 0, height: 0, width: 0 };
 
-function getIndexInParent(node: SceneNode) {
-	return [...node.parent.children].reverse().findIndex((child) => node.id === child.id);
-}
+// function getIndexInParent(node: SceneNode) {
+// 	return [...node.parent.children].reverse().findIndex((child) => node.id === child.id);
+// }
