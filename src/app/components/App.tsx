64d02f18f90f502757d3c7cc10d745364e4abfdc
James Bradford <j.bradford@pnnl.gov>
2022-09-23 15:35:16 -0700

    add 'minimal' UI for watch mode

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -1,30 +1,13 @@
 import * as React from 'react';
-import { FigmaPluginMessage, Message, CommandType } from '../../messages';
-// import '../styles/ui.css';
+import '../styles/ui.css';
 
 const App = ({}) => {
-	// const textbox = React.useRef<HTMLInputElement>(undefined);
-
-	// const countRef = React.useCallback((element: HTMLInputElement) => {
-	//     if (element) element.value = '5';
-	//     textbox.current = element;
+	// const runCommand = React.useCallback((type: CommandType) => {
+	// 	const message: FigmaPluginMessage<Message> = {
+	// 		pluginMessage: { type },
+	// 	};
+	// 	parent.postMessage(message, '*');
 	// }, []);
-
-	// const onCreate = () => {
-	//     const count = parseInt(textbox.current.value, 10);
-	//     parent.postMessage({pluginMessage: {type: 'create-rectangles', count}}, '*');
-	// };
-
-	// const onCancel = () => {
-	//     parent.postMessage({pluginMessage: {type: 'cancel'}}, '*');
-	// };
-
-	const runCommand = React.useCallback((type: CommandType) => {
-		const message: FigmaPluginMessage<Message> = {
-			pluginMessage: { type },
-		};
-		parent.postMessage(message, '*');
-	}, []);
 
 	React.useEffect(() => {
 		// This is how we read messages sent from the plugin controller
@@ -40,12 +23,18 @@ const App = ({}) => {
 
 	return (
 		<div>
-			<button onClick={() => runCommand('constrainSelection')} children="Auto Constrain Selection" />
+			{/* <h4>Watch Mode is Running</h4> */}
+			<p>
+				Auto Constraints will be applied to every object selected, both before and after it's touched. Closing
+				this window or running another plugin will quit Watch Mode.
+			</p>
+			{/* <button onClick={() => runCommand('constrainSelection')} children="Auto Constrain Selection" />
 			<button onClick={() => runCommand('constrainChildren')} children="Auto Constrain Children" />
 			<button onClick={() => runCommand('constrainDescendants')} children="Auto Constrain Descendants" />
 			<button onClick={() => runCommand('frameAndConstrainSelection')} children="Frame And Constrain Selection" />
-			<button onClick={() => runCommand('ignoreSelection')} children="Ignore Selection" />
+			<button onClick={() => runCommand('ignoreSelection')} children="Ignore Selection" /> */}
 			{/* don't apply to root frames */}
+			{/* <button children={'Hide this UI'} /> */}
 		</div>
 	);
 };
